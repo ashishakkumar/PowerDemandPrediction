9547b0cda388c45c1f3248a607b5b7bec9acb5b4
Ashish Kumar <86063242+ashishakkumar@users.noreply.github.com>
2023-06-27 01:17:02 +0530

    Update power.py

--- power.py
+++ power.py
@@ -11,22 +11,27 @@ from sklearn.metrics import mean_squared_error
 import plotly.express as px
 import urllib.request
 import datetime
-url = "https://github.com/ashishakkumar/PowerDemandPrediction/raw/main/multivariate_lstm.h5"
+url = "https://github.com/ashishakkumar/PowerDemandPrediction/raw/main/multivariate_lstm2.h5"
 filename = "multivariate_lstm.h5"
 urllib.request.urlretrieve(url, filename)
 
-
-page_bg_img = f"""
+ 
+page_bg_img = f'''
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://raw.githubusercontent.com/ashishakkumar/PowerDemandPrediction/main/340966496_570385771749371_2312951338874449866_n%20(1).jpg");
 background-size: cover;
-background-filter: blur(50px);
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
 }}
 </style>
-"""
-
+'''
 st.markdown(page_bg_img, unsafe_allow_html=True)
+
 
 
 
@@ -60,24 +65,29 @@ def multivariate_data(dataset, target, start_index, end_index, history_size,
 
 def main():
 
-
-    st.title(':black[Power Demand Prediction]')
-    current_date = datetime.date.today()
+    
+    st.title(':black[Power Demand Prediction (AI4ICPS)]')
+    date_str = "2023-05-14"
+    last_date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
 
     st.markdown("_Tips : Click on the same date twice in the calendar interface to see the prediction for single day_")
 
     selected_date = st.date_input(
         "Select a date for prediction",
-        value=(current_date, current_date),
-        min_value=current_date,
-        max_value=current_date + datetime.timedelta(days=365)
+        value=(last_date, last_date)
+       
     )
 
     if isinstance(selected_date, tuple):
         start_date, end_date = selected_date
     else:
         start_date, end_date = selected_date, selected_date
+     
 
+    start_date = datetime.datetime.strptime(str(start_date), "%Y-%m-%d").date()
+    end_date = datetime.datetime.strptime(str(end_date), "%Y-%m-%d").date()
+    num_days = (end_date - start_date).days +1
+    num_days_2 = (end_date - last_date).days +1
     st.markdown(f"You have selected: **{start_date}** to **{end_date}** for prediction.")
 
     merged_df = pd.read_csv("https://raw.githubusercontent.com/ashishakkumar/PowerDemandPrediction/main/ap_dataset/APDATA.csv",
@@ -104,9 +114,10 @@ def main():
     y = merged_df['Energy Required (MU)'].values
     y = y.reshape(-1, 1)
 
+    #defining the end indices of train validation and test set
     train_end_idx = 1202
-    cv_end_idx = 1412
-    test_end_idx = 1594
+    cv_end_idx = 1367
+    test_end_idx = 1565
 
     scaler_X = MinMaxScaler(feature_range=(0, 1))
     scaler_y = MinMaxScaler(feature_range=(0, 1))
@@ -119,20 +130,20 @@ def main():
 
     dataset_norm = np.concatenate((X_norm, y_norm), axis=1)
     n_features = 6
-    past_history = 15
-    future_target = 1
+    past_history = 60
+    future_target = 30
 
     X_train, y_train = multivariate_data(dataset_norm[:, 0:-1], dataset_norm[:, -1],
                                          0, train_end_idx, past_history, 
-                                         future_target, step=1, single_step=True)
+                                         future_target, step=1, single_step=False)
 
     X_val, y_val = multivariate_data(dataset_norm[:, 0:-1], dataset_norm[:, 0],
                                      train_end_idx, cv_end_idx, past_history, 
-                                     future_target, step=1, single_step=True)
+                                     future_target, step=1, single_step=False)
 
     X_test, y_test = multivariate_data(dataset_norm[:, 0:-1], dataset_norm[:, 0],
                                        cv_end_idx, test_end_idx, past_history, 
-                                       future_target, step=1, single_step=True)
+                                       future_target, step=1, single_step=False)
 
     batch_size = 32
     buffer_size = 1184
@@ -143,16 +154,8 @@ def main():
     validation = tf.data.Dataset.from_tensor_slices((X_val, y_val))
     validation = validation.batch(batch_size).prefetch(1)
 
-    input_shape = X_train.shape[-2:]
-    loss = tf.keras.losses.MeanSquaredError()
-    metric = [tf.keras.metrics.RootMeanSquaredError()]
-    lr_schedule = tf.keras.callbacks.LearningRateScheduler(
-                  lambda epoch: 1e-4 * 10 ** (epoch / 10))
-    early_stopping = tf.keras.callbacks.EarlyStopping(patience=10)
 
-
-
-    y_test = y_test.reshape(-1, 1)
+    
     y_test_inv = scaler_y.inverse_transform(y_test)
     test_forecast = multivariate_lstm.predict(X_test)
     lstm_forecast = scaler_y.inverse_transform(test_forecast)
@@ -170,8 +173,15 @@ def main():
     X_test2 = scaler_X.transform(X[start_idx:end_idx, :])
     X_test2 = X_test2.reshape(1, past_history, n_features)
     forecast_unseen = multivariate_lstm.predict(X_test2)
-    forecast_unseen_inverse = scaler_y.inverse_transform(forecast_unseen)
-    st.write("Unseen data forecast:", forecast_unseen_inverse)
+    forecast_unseen_inverse = scaler_y.inverse_transform(forecast_unseen)[0]
+    date_range = pd.date_range(start=start_date, periods=(end_date -start_date).days+1, freq='D')
+    # Create an empty DataFrame
+    df = pd.DataFrame()
+
+    # Add the Date column to the DataFrame
+    df['Date'] = date_range
+    df['Forecasted Demand (MU)'] = forecast_unseen_inverse[(start_date-last_date).days:(end_date-last_date).days+1]
+    st.bar_chart(data=df, x='Date', y='Forecasted Demand (MU)')
 
 if __name__ == "__main__":
     main()
